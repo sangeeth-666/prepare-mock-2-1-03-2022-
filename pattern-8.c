8ab841c7d2d7117ed99ddf80294e15ea9a707e0c
sangeeth <sangeeth@gmail.com>
2022-03-01 18:26:44 +0530

    "pattern-8"

---
+++ pattern-8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=5,i,j,k;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=n-1;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" ");
+        }
+        for(k=i;k<=n-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
