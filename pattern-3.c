e82591c9d6d8186fb9ea43cf9685e21465a3ed36
sangeeth <sangeeth@gmail.com>
2022-03-01 18:19:01 +0530

    "pattern-3"

---
+++ pattern-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n=5,i,j,k;
+    for(i=1;i<=n;i++)
+    {
+        for(j=n;j>=i;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
